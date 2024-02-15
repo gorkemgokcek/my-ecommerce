25e2ac110da3d98e56a5b3c61ddf135099c3c9b6
Gorkem Gokcek <gorkemgokcekk@gmail.com>
2024-02-15 13:57:08 +0300

    DataContext add, Carousel1 add.

---
+++ src/contexts/DataContext.js
@@ -0,0 +1,44 @@
+//import { images } from '../assets/images';
+
+export const data = {
+    header: {
+        phone: "(225) 555-0118",
+        mail: "michelle.rivera@example.com",
+        message: "Follow Us and get a chance to win 80% off",
+        socialURL: {
+            instagram: "instagram.com",
+            youtube: "youtube.com",
+            facebook: "facebook.com",
+            twitter: "twitter.com"
+        },
+        firstName: "GGStore",
+    },
+    home:
+    {
+        heroWomen: {
+            slides:
+            [
+                {
+                    h5: "SUMMER 2020",
+                    h1: "NEW COLLECTION",
+                    h4: "We know how large objects will act,",
+                    h6: " but things on a small scale.",
+                    src: images.home.heroWomen.heroWomenOne,
+                    key: 1,
+                    altText: 'Summer 2020 Products',
+                },
+                {
+                    h5: "WINTER 2020",
+                    h1: "NEW COLLECTION",
+                    h4: "We know how large objects will act,",
+                    h6: " but things on a small scale.",
+                    src: images.home.heroWomen.heroWomenTwo,
+                    key: 2,
+                    altText: 'Winter 2020 Products',
+                },
+            ]
+      
+    }
+}
+
+}
