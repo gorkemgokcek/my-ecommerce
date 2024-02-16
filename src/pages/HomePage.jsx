0f6237c3b15e265226d8d83897a45b1503b7b75f
Gorkem Gokcek <gorkemgokcekk@gmail.com>
2024-02-16 15:26:24 +0300

    homepage fixed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,21 +2,19 @@ import SliderCarousel from "../components/SliderCarousel";
 export default function HomePage(){
     return (
         <div>
-        <div className="relative text-white">
+        <div className="relative text-white hero-section">
             <SliderCarousel />
-            <div className="absolute top-0 left-0 z-10 flex flex-col text-left py-[40px] gap-[24px] ml-20">
-                <div><h5 className="font-bold text-[16px] font-container leading-[24px]">SUMMER 2020</h5></div>
+            <div className="absolute top-0 left-0 z-10 flex flex-col xl:text-left 2xl:text-left lg:text-left md:text-left sm:text-left xl:py-[290px] lg:py-[180px] md:py-[120px] sm:py-[100px] py-[40px] gap-[1px] 2xl:gap-[24px] xl:gap-[24px] lg:gap-[18px] md:gap-[14px] sm:gap-[12px] xl:ml-[200px] 2xl:ml-[200px] lg:ml-[120px] md:ml-[80px] sm:ml-[60px] ml-[30px]">
+                    <div><h5 className="font-2xl:bold 2xl:text-[16px] xl:text-[16px] lg:text-sm md:text-xs sm:text-xs text-[16px]  leading-[24px]">SUMMER 2020</h5></div>
                     <div>
-                        <h1 className="font-bold text-[58px] font-container leading-[80px]">NEW COLLECTION</h1>
-                        <br/>
-                        <h1 className="font-bold text-[58px] font-container leading-[80px]"></h1>
-                    </div>
-                    <div><h4 className=" text-[20px] leading-[30px] font-container">We know how large objects will act,</h4>
-                    <h6 className="text-[20px] leading-[30px] font-container">but things on a small scale.</h6></div>
-                        <button className="text-[24px] font-bold text-white bg-[#2DC071] rounded-[5px] py-3 px-2 w-[50%]">SHOP NOW</button>
-                </div>
-            </div>
+                        <h1 className="font-bold xl:text-[58px] 2xl:text-[58px] lg:text-[40px] md:text-[32px] sm:text-[28px] text-[40px]  leading-[80px]">NEW COLLECTION </h1>
 
+                    </div>
+                    <div><h4 className="2xl:text-xl xl:text-xl leading-[30px] ">We know how large objects will act,</h4>
+                        <h4 className="2xl:text-xl xl:text-xl  leading-[30px] ">but things on a small scale.</h4></div>
+                    <div><button className="carousel-button hover:bg-white hover:text-[#2DC071]">SHOP NOW</button></div>
+                </div>
+              </div>
             <div className="editor-pick py-[80px] relative">
              <h3 className="font-bold text-[24px] leading-[32px] font-container text-[#252B42] align-middle mb-3">EDITOR'S PICK</h3>
              <p className="text-[20px] leading-[30px] align-middle">Problems trying to resolve the conflict between. </p>
