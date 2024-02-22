c32c84cba024105406c84dabd505911ba4157125
Gorkem Gokcek <gorkemgokcekk@gmail.com>
2024-02-22 17:41:57 +0300

    Team Page fixed

--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -5,7 +5,7 @@ export default function Team() {
         <div className="flex-col flex items-center mt-10 pb-10">
             <div className="w-[400px] mb-5 flex flex-col gap-3">
                 <h2 className="font-bold text-4xl text-center">Meet Our Team</h2>
-                <p className="text-md leading-5 text-secondaryColor text-center">Problems trying to resolve the conflict between the two major realms of Classical physics</p>
+                <p className="text-md leading-5 text-secondaryColor text-center">Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics</p>
             </div>
             <div className="flex flex-col md:flex-row gap-4 flex-wrap">
                 <TeamCard />
